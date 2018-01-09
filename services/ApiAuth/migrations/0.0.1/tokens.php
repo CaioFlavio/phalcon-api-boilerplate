2ea8e885b6e4e600f3250acd36c2239179330df4
Caio Flavio <caioflavio2@hotmail.com.br>
2018-01-09 17:23:44 -0200

    [0.0.1][added]: table migrations

---
+++ services/ApiAuth/migrations/0.0.1/tokens.php
@@ -0,0 +1,113 @@
+<?php
+use Phalcon\Db\Column as Column;
+use Phalcon\Db\Index as Index;
+use Phalcon\Config\Adapter\Ini;
+use Phalcon\Db\Reference as Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class TokensMigration_1 extends Migration
+{
+    public function up()
+    {
+        $config = new Ini(dirname(__DIR__) . '\..\..\..\app\config.ini');
+        $this->morphTable(
+            'tokens',
+            [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type'          => Column::TYPE_INTEGER,
+                            'size'          => 10,
+                            'unsigned'      => true,
+                            'notNull'       => true,
+                            'autoIncrement' => true,
+                            'first'         => true,
+                        ]
+                    ),
+                    new Column(
+                        'user_id',
+                        [
+                            'type'     => Column::TYPE_INTEGER,
+                            'size'     => 10,
+                            'unsigned' => true,
+                            'notNull'  => true,
+                            'after'    => 'id',
+                        ]
+                    ),
+                    new Column(
+                        'token',
+                        [
+                            'type'     => Column::TYPE_VARCHAR,
+                            'size'     => 255,
+                            'unsigned' => true,
+                            'notNull'  => true,
+                            'after'    => 'secret_key',
+                        ]
+                    ),
+                    new Column(
+                        'expires_at',
+                        [
+                            'type'    => Column::TYPE_DATETIME,
+                            'notNull' => true,
+                            'after'   => 'token',
+                        ]
+                    ),
+                    new Column(
+                        'created_at',
+                        [
+                            'type'    => Column::TYPE_DATETIME,
+                            'notNull' => true,
+                            'after'   => 'expires_at',
+                        ]
+                    ),
+                    new Column(
+                        'updated_at',
+                        [
+                            'type'    => Column::TYPE_DATETIME,
+                            'notNull' => true,
+                            'after'   => 'created_at',
+                        ]
+                    ),
+                    new Column(
+                        'active',
+                        [
+                            'type'    => Column::TYPE_INTEGER,
+                            'size'    => 1,
+                            'notNull' => true,
+                            'after'   => 'updated_at',
+                        ]
+                    ),
+                ],
+                'indexes' => [
+                    new Index(
+                        'PRIMARY',
+                        [
+                            'id',
+                        ]
+                    ),
+                ],
+                'references' => [
+                    new Reference(
+                        'user_id_fk',
+                        [
+                            'referencedSchema'  => $config->database->dbname,
+                            'referencedTable'   => 'users',
+                            'columns'           => [
+                                'user_id',
+                            ],
+                            'referencedColumns' => [
+                                'id',
+                            ],
+                        ]
+                    ),
+                ],
+                'options' => [
+                    'TABLE_TYPE'      => 'BASE TABLE',
+                    'ENGINE'          => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci',
+                ],
+            ]
+        );     
+    }
+}
